9a8ab2b02e155bc5dc88948636a0fe5191f0ba24
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-12-11 13:31:19 -0500

    Set up links in NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>
+    <nav>
         <h1>Nav Bar</h1>
-    </div>
+        <RouterLink to="/" name="NewsFeed">The Buzz</RouterLink>
+        <RouterLink to="/explore" name="ExplorePage">Pollination Station</RouterLink>
+        <RouterLink :to="{name: 'ProfilePage', params: {user_id: userId}}" name="ProfilePage">Profile</RouterLink>
+        <RouterLink to="/login" name="LoginPage">Login</RouterLink>
+    </nav>
 </template>
 
 <script>
 export default {
-    name: 'NavBar'
+    name: 'NavBar',
+    data: () => ({
+        userId: 1,
+    }),
+    methods: {
+        
+    }
 }
 </script>
 
