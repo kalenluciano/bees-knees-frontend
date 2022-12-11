2f151bbf5f221c5d615bea80b618a41901021b7a
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-12-11 14:26:20 -0500

    Set up check session functionality on App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,40 @@
 
 <script>
 import NavBar from './components/NavBar'
+import { CheckSession } from './services/Auth'
 
 export default {
   name: 'App',
   components: {
     NavBar
+  }, 
+  data: () => ({
+    user: null,
+    authenticated: false
+  }),
+  mounted: function() {
+    const token = localStorage.getItem('token')
+    if (token) {
+      this.checkToken()
+    }
+  },
+  methods: {
+    setUser(user) {
+      this.user = user
+    },
+    setAuthenticated(state) {
+      this.authenticated = state
+    },
+    handleLogOut() {
+      this.setUser(null)
+      this.setAuthenticated(false)
+      localStorage.clear()
+    },
+    async checkToken() {
+      const user = await CheckSession()
+      this.setUser(user)
+      this.setAuthenticated(true)
+    }
   }
 }
 </script>
--- src/services/Auth.js
+++ src/services/Auth.js
@@ -1,29 +1,17 @@
 import Client from './api';
 
 export const LoginUser = async (data) => {
-	try {
-		const res = await Client.post('/auth/login');
-		localStorage.setItem('token', res.data.token);
-		return res.data.user;
-	} catch (error) {
-		throw error;
-	}
+	const res = await Client.post('/auth/login');
+	localStorage.setItem('token', res.data.token);
+	return res.data.user;
 };
 
 export const SignUpUser = async (data) => {
-	try {
-		const res = await Client.post('/auth/register');
-		return res.data;
-	} catch (error) {
-		throw error;
-	}
+	const res = await Client.post('/auth/register');
+	return res.data;
 };
 
 export const CheckSession = async () => {
-	try {
-		const res = await Client.get('/auth/session');
-		return res.data;
-	} catch (error) {
-		throw error;
-	}
+	const res = await Client.get('/auth/session');
+	return res.data;
 };
