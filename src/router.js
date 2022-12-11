5ce604f8fd00d28a46de2207ce018af64724d035
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-12-11 12:44:36 -0500

    Created routes

--- src/router.js
+++ src/router.js
@@ -1,1 +1,21 @@
 import { createWebHistory, createRouter } from 'vue-router';
+import Explore from './pages/Explore';
+import Login from './pages/Login';
+import NewsFeed from './pages/NewsFeed';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+
+const routes = [
+	{ path: '/', component: NewsFeed, name: 'NewsFeed' },
+	{ path: '/explore', component: Explore, name: 'Explore' },
+	{ path: '/profile/:user_id', component: Profile, name: 'Profile' },
+	{ path: '/login', component: Login, name: 'Login' },
+	{ path: '/sign-up', component: SignUp, name: 'SignUp' }
+];
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes
+});
+
+export default router;
