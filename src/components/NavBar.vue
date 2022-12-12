482117f16decb7e3f8db7437383561b493cd646b
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2022-12-12 00:05:18 -0500

    Updated conditional rendering for nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,16 @@
 <template>
-    <nav>
-        <h1>Nav Bar</h1>
-        <RouterLink to="/" name="NewsFeed">The Buzz</RouterLink>
-        <RouterLink v-if="user.authenticated" to="/explore" name="ExplorePage">Pollination Station</RouterLink>
-        <RouterLink v-if="user.authenticated" :to="{name: 'ProfilePage', params: {user_id: user.user.id}}" name="ProfilePage">Profile</RouterLink>
-        <RouterLink v-if="!user.authenticated" to="/login" name="LoginPage">Login</RouterLink>
-        <RouterLink v-else to="/" name="NewsFeed" @click="handleLogOut" >Log Out</RouterLink>
-    </nav>
+    <div>
+        <nav v-if="user.authenticated">
+            <RouterLink to="/" name="NewsFeed">The Buzz</RouterLink>
+            <RouterLink to="/explore" name="ExplorePage">Pollination Station</RouterLink>
+            <RouterLink :to="{name: 'ProfilePage', params: {user_id: user.user.id}}" name="ProfilePage">Profile</RouterLink>
+            <RouterLink to="/" name="NewsFeed" @click="handleLogOut" >Log Out</RouterLink>
+        </nav>
+        <nav v-else >
+            <RouterLink to="/login" name="LoginPage">Login</RouterLink>
+            <RouterLink to="/sign-up" name="SignUp">Sign Up</RouterLink>
+        </nav>
+    </div>
 </template>
 
 <script>
